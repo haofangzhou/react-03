dec329b4d76de94cc15592a04774a80d01183450
haofangzhou <haofangzhou@sinosoft.com.cn>
2017-08-17 19:25:04 +0800

    增加normalize.css

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+import 'normalize.css'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      newTodo:'test',
+      todoList:[
+        {id:1,title:'第一待办'},
+        {id:2,title:'第二待办'},
+        ]
+    }
+  }
+  render() {
+    let todos=this.state.todoList.map((item,index) =>{
+      console.log("a===="+item.title)
+      return(<li><TodoItem todo={item} /></li>) 
+    })
+    return (
+      <div className="App">
+           <h1>我的待办事项</h1>
+           <div className='inputWrapper'>
+              <TodoInput content={this.state.newTodo} />
+           </div>
+          <ol>
+            {todos}
+          </ol>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/TodoInput.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+export default class TodoInput extends Component{
+    submit(e){
+    if (e.key === 'Enter') {
+      this.props.onSubmit(e)
+        }
+   }
+   changeTitle(e){
+    this.props.onChange(e)
+  }
+    render(){
+       return <input type="text"  value={this.props.content}   onChange={this.changeTitle.bind(this)}
+       onKeyPress={this.submit.bind(this)}/>
+   }
+}
