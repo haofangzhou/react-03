e24f7e6d7abfb96373599cdf4b79e6ca34de88f1
haofangzhou <haofangzhou@sinosoft.com.cn>
2017-08-18 19:21:02 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -5,29 +5,64 @@ import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import 'normalize.css';
 
-
+let id=0
+function idMaker(){
+  id+=1
+  return id
+}
 class App extends Component {
   constructor(props){
     super(props)
     this.state ={
-      newTodo:'test',
-      todoList:[
-        {id:1,title:'第一待办'},
-        {id:2,title:'第二待办'},
-        ]
+      newTodo:'',
+      todoList:[ ]
     }
   }
-  render() {
-    let todos=this.state.todoList.map((item,index) =>{
-      return(<li><TodoItem todo={item} /></li>) 
+  addTodo(e){
+    this.state.todoList.push({
+      id:idMaker(),
+      title:e.target.value,
+      status:null,
+      deleted:false,
     })
+ 
+    this.setState({
+      newTodo:'',
+      todoList:this.state.todoList
+    })
+  }
+  changeTitle(e){
+    console.log("e.target.value="+e.target.value)
+    this.setState({
+      newTodo:e.target.value,
+      todoList:this.state.todoList
+    })
+  }
+  toggle(e,todo){
+    todo.status=todo.status==='completed'? '':'completed'
+    this.setState(this.state)
+  }
+  delete (event,todo){
+    todo.deleted=true
+    this.setState(this.state)
+  }
+  render() {
+    let todos=this.state.todoList.filter((item)=>!item.deleted).map((item,index) =>{
+      console.log("222");
+        return(
+          <li key={index}>
+            <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+            onDelete={this.delete.bind(this)} />
+          </li>) 
+    })
+    console.log("todo=="+todos);
     return (
       <div className="App">
            <h1>我的待办事项</h1>
            <div className='inputWrapper'>
-              <TodoInput content={this.state.newTodo} />
+           待办事项:&nbsp;&nbsp;<TodoInput content={this.state.newTodo}   onSubmit={this.addTodo.bind(this)} onChange={this.changeTitle.bind(this)} />
            </div>
-          <ol>
+          <ol className='todoList'>
             {todos}
           </ol>
       </div>
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,16 +1,21 @@
 import React,{Component} from 'react';
 
 export default class TodoInput extends Component{
-    submit(e){
+
+  submit(e){
     if (e.key === 'Enter') {
+      console.log("e=="+e);
       this.props.onSubmit(e)
-        }
-   }
+    }
+ }
    changeTitle(e){
     this.props.onChange(e)
   }
     render(){
-       return <input type="text"  value={this.props.content}   onChange={this.changeTitle.bind(this)}
-       onKeyPress={this.submit.bind(this)}/>
+       return <input type="text"  className="TodoInput" value={this.props.content} placeholder="请输入"
+       onKeyPress={this.submit.bind(this)} 
+       onChange={this.changeTitle.bind(this)}
+       />
    }
+ 
 }
